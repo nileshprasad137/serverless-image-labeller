dbba6d171034be884838db3340135ff769bc25d3
Nilesh Prasad <prasadnilesh96@gmail.com>
2020-08-16 14:29:23 +0530

    label gettn\ing detected

--- handlers/handler.py
+++ handlers/handler.py
@@ -1,4 +1,6 @@
 import json
+import boto3
+import os
 
 
 def labelOnS3Upload(event, context):
@@ -7,12 +9,31 @@ def labelOnS3Upload(event, context):
         "input": event
     }
 
-    response = {
+    bucket = os.environ['SERVERLESS_IMAGE_LABELLING_BUCKET']
+    region_name = os.environ['REGION_NAME']
+
+    filesUploaded = event['Records']
+    for file in filesUploaded:
+        fileName = file["s3"]["object"]["key"]
+        rekognitionClient = boto3.client('rekognition', region_name="us-east-1")
+        response = rekognitionClient.detect_labels(Image={'S3Object':{'Bucket':bucket,'Name':fileName}},
+            MaxLabels=10)
+        print(response)
+
+        print('Detected labels for ' + fileName)
+        print()
+        for label in response['Labels']:
+            print ("Label: " + label['Name'])
+            print ("Confidence: " + str(label['Confidence']))
+
+        print(len(response['Labels']))
+
+    finalResponse = {
         "statusCode": 200,
         "body": json.dumps(body)
     }
-
-    return response
+#     print(finalResponse)
+    return finalResponse
 
     # Use this code if you don't use the http event with the LAMBDA-PROXY
     # integration
